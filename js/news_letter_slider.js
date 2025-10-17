8dad3b3bd3aa6ebc99565ec65e3131abdbb7c64d
KANGNAGYEONG <nnak1606@gmail.com>
2025-10-17 11:52:24 +0900

    edit

--- js/news_letter_slider.js
+++
@@ -1,5 +0,0 @@
-var swiper = new Swiper(".news-letter-list", {
-  slidesPerView: "auto",
-  loop: false,
-  grabCursor: true
-});
--- js/program_project.js
+++
@@ -1,4 +0,0 @@
-const lightbox = GLightbox({
-  selector: '.glightbox'
-  // data-gallery 속성으로 그룹 관리됨
-});

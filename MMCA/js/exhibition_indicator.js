2f389f82b666f9b9725ebf5360b1a4980ce4904c
KANGNAGYEONG <nnak1606@gmail.com>
2025-10-23 14:48:37 +0900

    edit

--- MMCA/js/exhibition_indicator.js
+++ MMCA/js/exhibition_indicator.js
@@ -1,39 +1,3 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//   // 모든 exhibition 섹션 선택
-//   const exhibitions = document.querySelectorAll('.exhibition');
-
-//   exhibitions.forEach(exhibition => {
-//     const exhibitionList = exhibition.querySelector('.exhibition-list');
-//     const slides = exhibition.querySelectorAll('.exhibition-itme');
-//     const indicators = exhibition.querySelectorAll('.indicator span');
-
-//     if (!exhibitionList || slides.length === 0 || indicators.length === 0) return;
-
-//     let slideWidth = slides[0].offsetWidth;
-
-//     // 인디케이터 클릭 이벤트
-//     indicators.forEach((indicator, idx) => {
-//   indicator.addEventListener('click', () => {
-//     // transition 켜둔 상태에서 transform만 변경
-//     exhibitionList.style.transition = 'transform 0.5s ease';
-//     exhibitionList.style.transform = `translateX(-${slideWidth * idx}px)`;
-
-//     indicators.forEach(ind => ind.classList.remove('on'));
-//     indicator.classList.add('on');
-//   });
-// });
-
-
-//     // 창 크기 변경 시 슬라이드 너비 갱신
-//     window.addEventListener('resize', () => {
-//       slideWidth = slides[0].offsetWidth;
-//       const activeIdx = [...indicators].findIndex(ind => ind.classList.contains('on'));
-//       exhibitionList.style.transform = `translateX(-${slideWidth * activeIdx}px)`;
-//     });
-//   });
-// });
-
-
 const swiper3 = new Swiper('.exhibition-seou', {
   // Optional parameters
   direction: 'horizontal',
@@ -61,11 +25,40 @@ const swiper4 = new Swiper('.exhibition-gwacheon', {
 const swiper5 = new Swiper('.exhibition-deoksugung', {
   // Optional parameters
   direction: 'horizontal',
-  loop: true,
+  // loop: true,
+  // 슬라이더 1개
 
   // If we need pagination
   pagination: {
     el: '.pagination-deoksugung',
     clickable: true,
   },
+
+  watchOverflow: false,
+});
+
+const swiper6 = new Swiper('.exhibition-cheongju', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.pagination-cheongju',
+    clickable: true,
+  },
+});
+
+const swiper7 = new Swiper('.exhibition-children', {
+  // Optional parameters
+  direction: 'horizontal',
+  // loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.pagination-children',
+    clickable: true,
+  },
+
+  watchOverflow: false,
 });
--- MMCA/js/sub_exhibition.js
+++ MMCA/js/sub_exhibition.js
@@ -1,6 +1,6 @@
 // 초기 설정
 $('.exhibition-card').hide();
-$('.exhibition-card[date-category="all"]').css('display', 'flex');
+$('.exhibition-card[date-category="all"]').show();
 
 $('.exhibition-page .exhibition-filter li').click(function(){
   $('.exhibition-page .exhibition-filter li').removeClass('on');
@@ -11,13 +11,13 @@ $('.exhibition-page .exhibition-filter li').click(function(){
   
   if (exhibition === 'all') {
     // 전체 exhibition-card 숨기고
-    $('.exhibition-card').hide();
+    $('.exhibition-card').hide().css({'opacity' : 0});
     // 전체를 눌렀을 때 date-category="all"만 보이게
-    $('.exhibition-card[date-category="all"]').css({'display' : 'flex'});
+    $('.exhibition-card[date-category="all"]').show().animate({opacity : 1}, 500);
   } else {
-    $('.exhibition-card').hide(); 
+    $('.exhibition-card').hide().css({'opacity' : 0}); 
     // 모두 숨기고
-    $('.exhibition-card[date-category="' + exhibition + '"]').css({'display' : 'flex'});
+    $('.exhibition-card[date-category="' + exhibition + '"]').show().animate({opacity : 1}, 500);
     // date-fiter와 이름이 같은 exhibition-card만 보이게
   }
 });
